38cfebed4cf36dea0dbebb3bbace315c973f2b3c
jeriabegglen <jeriabegglen@gmail.com>
2016-10-27 11:22:10 -0600

    set up of problem to solve with Collections

---
+++ src/myjavahomework/MyJavaHomework.java
@@ -0,0 +1,32 @@
+/*
+ Jeri Abegglen
+Use collections to convert seconds into hours minutes and seconds
+ */
+package myjavahomework;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MyJavaHomework {
+        
+    public static void main(String[] args) {
+        Collection<Integer> value = new ArrayList<Integer>();
+        
+        value.add(9630);
+        value.add(8955);
+        value.add(4766);
+        value.add(5001);
+        
+        System.out.println("Seconds converter to hours:minutes:seconds ");
+        int hours = 0, minutes = 0, seconds = 0, remainder = 0;
+        for(int i : value){
+            hours = i / 3600;
+            remainder = i % 3600;
+            minutes = remainder / 60;
+            seconds = remainder % 60;
+            System.out.println(i + " seconds = " + hours + ":" + minutes + ":" + seconds + ":");
+        }        
+    }
+}
+
